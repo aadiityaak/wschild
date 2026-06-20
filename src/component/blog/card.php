f0e5a79dfdc5e423902e0025ad5e32efd205e6fb
aadiityaak <aadiityaak@gmail.com>
2026-06-20 22:00:33 +0700

    feat: update blog card and add responsive archive grid
    - Restructure blog card HTML to combine date section and readmore link into single footer
    - Replace text readmore with accessible circular arrow icon button with hover states
    - Update footer CSS for space-between alignment and proper styling
    - Add responsive two-column grid for blog archives on screens ≥480px, single column on mobile under 480px

--- src/component/blog/card.php
+++ src/component/blog/card.php
@@ -48,18 +48,11 @@
 				<?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?>
 			</div>
 
-			<div class="wschild-blog-card__date-row">
-				<svg class="wschild-blog-card__date-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-					<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-				</svg>
+			<div class="wschild-blog-card__footer">
 				<time class="wschild-blog-card__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
 					<?php echo get_the_date(); ?>
 				</time>
-			</div>
-
-			<div class="wschild-blog-card__footer">
-				<a href="<?php the_permalink(); ?>" class="wschild-blog-card__readmore">
-					Baca Selengkapnya
+				<a href="<?php the_permalink(); ?>" class="wschild-blog-card__arrow" aria-label="Baca selengkapnya">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
 					</svg>
